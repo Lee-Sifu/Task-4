903a5b22eaec134a812f55ddff510f86eb6d3556
Jason Lee <jason.m.lee.2003@gmail.com>
2026-02-19 16:04:11 -0500

    Added nut img

--- CART263-TASK-4/js/nut.js
+++ CART263-TASK-4/js/nut.js
@@ -6,6 +6,16 @@ class nut {
         this.nutColor = nutColor;
     }
     renderNut() {
-        
+        // create an <img> element to render the nut image
+        let nutImg = document.createElement("img");
+        nutImg.classList.add("nut");
+        // path relative to index.html
+        nutImg.src = "img/nut.svg";
+        nutImg.style.width = this.size + "px";
+        nutImg.style.height = this.size + "px";
+        nutImg.style.position = "absolute";
+        nutImg.style.left = this.x + "px";
+        nutImg.style.top = this.y + "px";
+        document.getElementsByClassName("grass")[0].appendChild(nutImg);
     }
 }
