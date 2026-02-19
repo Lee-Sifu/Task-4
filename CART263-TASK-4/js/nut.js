b6cdc5cb8ac9cd0e0755096427ea06de345533b3
Jason Lee <jason.m.lee.2003@gmail.com>
2026-02-19 15:42:18 -0500

    Added renderNut

--- CART263-TASK-4/js/nut.js
+++ CART263-TASK-4/js/nut.js
@@ -1,7 +1,15 @@
 class nut {
-    constructor(x, y, nutColor) {
+    constructor(x, y, size,nutColor) {
         this.x = x;
         this.y = y;
+        this.size = size;
         this.nutColor = nutColor;
+    }
+    renderNut() {
+        //nut - IN the sky
+        this.nutDiv.classList.add("nut");
+        this.nutDiv.style.background = `rgb(${this.nutColor.r},${this.nutColor.g},${this.nutColor.b})`;
+        //append to the SKY div
+        document.querySelector(".sky").appendChild(this.nutDiv);
     }
 }
